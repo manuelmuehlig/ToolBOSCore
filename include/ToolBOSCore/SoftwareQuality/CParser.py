cd75a94a0acf803990bf5975bddf76e5440f8324
Marijke Stein <marijke.stein@honda-ri.de>
2021-07-21 10:56:14 +0200

    [TBCORE-2046] CParser.py: re-indent + added debug messages

--- include/ToolBOSCore/SoftwareQuality/CParser.py
+++ include/ToolBOSCore/SoftwareQuality/CParser.py
@@ -41,59 +41,59 @@ from clang.cindex import CursorKind, TypeKind
 
 from ToolBOSCore.Platforms                       import Platforms
 from ToolBOSCore.Settings                        import ToolBOSConf
-from ToolBOSCore.SoftwareQuality.CParserElements import MacroDefinition,\
+from ToolBOSCore.SoftwareQuality.CParserElements import MacroDefinition, \
                                                         MacroFnDefinition, \
                                                         Namespace
 from ToolBOSCore.Util                            import Any
 
 
 class CParser( Namespace ):
-    """Class for parsing C code to extract variable, struct, enum, and function declarations as well as preprocessor
-    macros. This is a LLVM/CLANG based C/C++ parser; it is meant to simplify the process of extracting definitions from
-    header files in the absence of a complete build system.
+    """
+        Class for parsing C code to extract variables, structs, enums, or
+        function declarations as well as preprocessor macros.
 
-    Usage:
-        ## create parser object, load two files
-        p = CParser('header1.h')
+        Usage:
+            # create parser object, load two files
+            p = CParser('header1.h')
 
-        ## just to see what was successfully parsed from the files
-        p.printAll()
+            # just see what was successfully parsed from the files
+            p.printAll()
 
-        ## access parsed declarations
-        macros   = p.defs['macros']
-        fnmacros = p.defs['fnmacros']
-        ...
+            # access parsed declarations
+            macros   = p.defs['macros']
+            fnmacros = p.defs['fnmacros']
     """
 
-    def __init__( self, filepath, isCPlusPlus, langStd, verbose=False, includepaths=None, defines=None, args=None ):
+    def __init__( self, filePath, isCPlusPlus, langStd,
+                  includepaths=None, defines=None, args=None ):
         """
-        Creates a new CParser instance.
-        The file is indexed at constructor time.
-        :param filepath: the file to parse
-        :param verbose: whether or not to print diagnostic information during parsing.
-        :param includepaths: a list of include paths. All the basic include
-                             paths must be specified, including at least:
-                             - the most basic include path, on unix usually /usr/include
-                             - the clang standard library include path. Distribution and clang version dependent.
-                               On Ubuntu Precise 64 with clang 3.4 this is /usr/lib/clang/3.4/include
-                             For C++, the C++ include paths are also required.
-                             On Precise 64 these are:
-                             - /usr/include/c++/4.6/
-                             - /usr/include/c++/4.6/x86_64-linux-gnu/
-        :param args: extra compiler flags
-        """
+            Creates a new CParser instance.
 
-        Any.requireIsFileNonEmpty(filepath)
+            The file is indexed at constructor time.
+
+            :param filePath:     the file to parse
+            :param includepaths: list of include paths
+            :param args:         extra compiler flags
+
+            All the basic include paths must be specified, including at least
+              * compiler default include paths (e.g. /usr/include)
+              * compiler version specific include paths, e.g.:
+                /usr/lib/clang/6.0/include
+                /usr/include/c++/7.5.0
+        """
+        Any.requireIsFileNonEmpty( filePath )
+
         includepaths       = includepaths or []
         systemIncludePaths = self._getSystemIncludePaths()
         defines            = defines or []
 
+        self.filepath      = filePath
         self.langStd       = langStd
         self.isCPlusPlus   = isCPlusPlus
-        self.verbose       = verbose
 
+        logging.debug( 'creating Clang-based indexer' )
         try:
-            self._index = cidx.Index.create( )
+            self._index = cidx.Index.create()
         except cidx.LibclangError:
 
             hostPlatform = Platforms.getHostPlatform()
@@ -108,46 +108,58 @@ class CParser( Namespace ):
                 logging.error( 'unsupported platform: %s', hostPlatform )
                 return
 
+            logging.debug( 'loading library: %s', libPath )
             Any.requireIsFileNonEmpty( libPath )
             cidx.Config.set_library_file( libPath )
-
             self._index = cidx.Index.create()
 
-        self.filepath   = filepath
+        logging.debug( 'indexer created: %s', self._index )
 
-        # build a list of include directory compile flags to pass to the parse method of the index.
+        # build a list of include directory compile flags to pass to the parse
+        # method of the index
         argsIncludeDirs = [ "-I{}".format( d ) for d in systemIncludePaths ]
         completeArgs    = argsIncludeDirs + (args or [ ])
         self.args       = completeArgs
+        options         = cidx.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD
 
-        translationUnit = self._index.parse( filepath,
-                                             options=cidx.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD,
-                                             args=completeArgs )
+        logging.debug( 'parsing translation unit: %s', filePath )
+        translationUnit = self._index.parse( filePath, options=options, args=completeArgs )
 
         self.translationUnit = translationUnit
 
         # call super with the translation unit cursor
+        logging.debug( 'creating namespace: %s', translationUnit.cursor )
         super( CParser, self ).__init__( translationUnit.cursor )
+        logging.debug( 'namespace created' )
 
         # macros are extracted separately as the clang C bindings - and thus
         # the python ones - do not provide helpers to extract the info we need.
-        self._populateMacros( filepath,
-                              list(systemIncludePaths) + list(includepaths),
+        self._populateMacros( filePath,
+                              list( systemIncludePaths ) + list( includepaths ),
                               defines,
                               langStd )
 
+        logging.debug( 'initialization done' )
+
+
     def _populateMacros( self, filepath, includepaths, defines, langStd ):
-        """Populates the macro definitions in defs['macros'] and defs['fnmacros']"""
+        """
+            Populates the macro definitions in defs['macros'] and defs['fnmacros']
+        """
+        logging.debug( 'calling clang_macroinfo.get_macros()' )
+
         macroTuples = clang_macroinfo.get_macros( filepath.encode( 'utf8' ),
-                                                  [p.encode( 'utf8' ) for p in includepaths],
+                                                  [ p.encode( 'utf8' ) for p in includepaths ],
                                                   True,
-                                                  [d.encode( 'utf8' ) for d in defines],
+                                                  [ d.encode( 'utf8' ) for d in defines ],
                                                   langStd.encode( 'utf8' ) )
+
+        logging.debug( 'calling clang_macroinfo.get_macros() finished' )
 
         macros   = { }
         fnmacros = { }
 
-        for mt in macroTuples.values( ):
+        for mt in macroTuples.values():
             # mt.args is None for simple macros, a list for function style macros.
             if mt.args is None:
                 macros[ mt.name ]   = MacroDefinition( mt.name, mt.body, mt.location )
@@ -157,11 +169,13 @@ class CParser( Namespace ):
         self.defs[ 'macros' ]   = macros
         self.defs[ 'fnmacros' ] = fnmacros
 
+
     def __repr__( self ):
         return 'TranslationUnit({})'.format( self.defs )
 
+
     def printAll( self ):
-        print( self._membersStr( ) )
+        print( self._membersStr() )
 
         def prn( title, collection ):
             ndashes = (72 - len( title ) - 2) / 2
@@ -169,14 +183,14 @@ class CParser( Namespace ):
             print( '// {} {} {}'.format( dashes, title, dashes ) )
             for x in collection:
                 print( x )
-                print( )
+                print()
 
-        prn( 'MACROS', self.macros.values( ) )
-        prn( 'MACROS FNs', self.fnmacros.values( ) )
+        prn( 'MACROS', self.macros.values() )
+        prn( 'MACROS FNs', self.fnmacros.values() )
+
 
     # noinspection PyUnresolvedReferences
     def getFunctionCalls( self, fname, node=None ):
-
         node   = node or self._cursor
         retval = set()
 
@@ -187,7 +201,7 @@ class CParser( Namespace ):
             retval.add( (node.displayname, node.location.line) )
 
         for child in node.get_children():
-            childFuncs =  self.getFunctionCalls( fname, child )
+            childFuncs = self.getFunctionCalls( fname, child )
             if childFuncs:
                 retval.update( childFuncs )
 
@@ -196,7 +210,6 @@ class CParser( Namespace ):
 
     # noinspection PyUnresolvedReferences
     def getVariableDeclarations( self, fname, node=None ):
-
         node   = node or self._cursor
         retval = set()
 
@@ -208,20 +221,20 @@ class CParser( Namespace ):
                                   TypeKind.ELABORATED):
                 retval.add( (node.displayname, node.type.spelling, node.location.line) )
             elif node.type.kind == TypeKind.CONSTANTARRAY:
-                retval.add( (node.displayname, node.type.get_array_element_type().spelling, node.location.line) )
+                retval.add( (node.displayname, node.type.get_array_element_type().spelling,
+                             node.location.line) )
             else:
                 retval.add( (node.displayname, node.type.spelling, node.location.line) )
 
-
         for child in node.get_children():
-            childDecls =  self.getVariableDeclarations( fname, child )
+            childDecls = self.getVariableDeclarations( fname, child )
             if childDecls:
                 retval.update( childDecls )
 
         return retval
 
-    def getFunctionPrototypesWithoutParameters( self, fname, node=None ):
 
+    def getFunctionPrototypesWithoutParameters( self, fname, node=None ):
         node   = node or self._cursor
         retval = set()
 
@@ -237,12 +250,12 @@ class CParser( Namespace ):
             # but simply as FUNCTIONPROTO. In order to find functions without
             # parameter we then have to look in the tokens, searching for an
             # open parens and making sure that it is not followed by a close parens.
-            toks      = list( n.spelling for n in node.get_tokens() )
+            toks = list( n.spelling for n in node.get_tokens() )
 
             if '(' in toks and node.kind == CursorKind.FUNCTION_DECL:
                 parensIdx = toks.index( '(' )
 
-                if toks[parensIdx + 1] == ')':
+                if toks[ parensIdx + 1 ] == ')':
                     retval.add( (node.displayname, node.location.line) )
 
         for child in node.get_children():
@@ -251,6 +264,7 @@ class CParser( Namespace ):
                 retval.update( childFuns )
 
         return retval
+
 
     @property
     def macros( self ):
@@ -309,8 +323,9 @@ class CParser( Namespace ):
         return self._locals( self.fnmacros, self.filepath )
 
     @property
-    def localNamespaces(self):
-        return self._localNamespaces(self.filepath)
+    def localNamespaces( self ):
+        return self._localNamespaces( self.filepath )
+
 
     def _getSystemIncludePaths( self ):
         """
@@ -318,7 +333,8 @@ class CParser( Namespace ):
         """
         from ToolBOSCore.BuildSystem.Compilers import getIncludePaths
 
-        return getIncludePaths( 'clang-%s' % clang_macroinfo.VERSION, 'c++' ) or getIncludePaths( 'clang', 'c++' )
+        return getIncludePaths( 'clang-%s' % clang_macroinfo.VERSION, 'c++' ) or \
+               getIncludePaths( 'clang', 'c++' )
 
 
 # EOF
